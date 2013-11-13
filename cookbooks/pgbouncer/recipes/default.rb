b490be85e4fba9cdf0cd3ea533afca9bb4696dfc
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-13 11:34:50 +0000

    Update default.rb

--- cookbooks/pgbouncer/recipes/default.rb
+++ cookbooks/pgbouncer/recipes/default.rb
@@ -1,20 +1,20 @@
-#package "pgbouncer"
+package "pgbouncer"
 
-#service "pgbouncer" do
-#  service_name "pgbouncer"
-#  supports :restart => true, :status => true, :reload => true
-#end
+service "pgbouncer" do
+  service_name "pgbouncer"
+  supports :restart => true, :status => true, :reload => true
+end
 
 
-#template "/etc/pgbouncer/pgbouncer.ini" do
-#source "pgbouncer.ini.erb"
-#notifies :restart, resources(:service => "pgbouncer"), :immediately
-#end
+template "/etc/pgbouncer/pgbouncer.ini" do
+source "pgbouncer.ini.erb"
+notifies :restart, resources(:service => "pgbouncer"), :immediately
+end
 
-#template "/etc/pgbouncer/userlist.txt" do
-#source "userlist.txt.erb"
-#notifies :restart, resources(:service => "pgbouncer"), :immediately
-#end
+template "/etc/pgbouncer/userlist.txt" do
+source "userlist.txt.erb"
+notifies :restart, resources(:service => "pgbouncer"), :immediately
+end
 
 bash "Change port from 5432 to 6432" do
 user "root"
