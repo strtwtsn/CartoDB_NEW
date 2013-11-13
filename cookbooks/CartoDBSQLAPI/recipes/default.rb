4e593d69c9a31cb7c4846add538810bae0454c8f
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-13 12:06:00 +0000

    Update default.rb

--- cookbooks/CartoDBSQLAPI/recipes/default.rb
+++ cookbooks/CartoDBSQLAPI/recipes/default.rb
@@ -2,7 +2,7 @@ bash "Do CartoDBSQL stuff" do
 user "root"
 code <<-EOH
 git clone git://github.com/CartoDB/CartoDB-SQL-API.git /home/ubuntu/cartodb/CartoDB-SQL-API
-cd /home/ubuntu/CartoDB-SQL-API
+cd /home/ubuntu/cartodb/CartoDB-SQL-API
 git checkout master
 npm install
 EOH
