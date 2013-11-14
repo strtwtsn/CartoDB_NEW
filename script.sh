337a722d02554cc7de31c9ab47b51403cbf55071
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 10:37:39 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
+
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install ruby1.9.1 rubygems ruby1.9.1-dev git
-gem install chef ruby-shadow --no-ri --no-rdoc
+sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev git libicu-dev build-essential
+\curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 1.9.2
+rvm use 1.9.2 --default
+gem install chef -v 11.6.0 ruby-shadow --no-ri --no-rdoc
 git clone --recursive https://github.com/CartoDB/cartodb20.git /home/ubuntu/cartodb/cartodb20
 sudo add-apt-repository -y ppa:cartodb/gis
 sudo add-apt-repository -y ppa:cartodb/nodejs
@@ -11,5 +16,4 @@ sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository -y ppa:mapnik/v2.1.0
 sudo apt-get -y update
 sudo git clone https://github.com/strtwtsn/CartoDB_NEW.git /var/chef/cartodb
-chef-solo -c /var/chef/cartodb/config/chefsolo.rb -j /var/chef/cartodb/roles/cartodb.json
-
+rvmsudo chef-solo -c /var/chef/cartodb/config/chefsolo.rb -j /var/chef/cartodb/roles/cartodb.json
