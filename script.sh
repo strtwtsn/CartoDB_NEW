1c3dc4a5b17451332ffb481757099660d0a665c8
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 15:41:06 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,23 @@
-
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev git libicu-dev build-essential
-\curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm install 1.9.2
-rvm use 1.9.2 --default
-gem install chef -v 11.6.0 ruby-shadow --no-ri --no-rdoc
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git checkinstall
+cd /usr/local/src
+sudo wget ftp://ftp.ruby-lang.org/pub/ruby/ruby-1.9.2-p320.tar.gz
+sudo tar -xvzf ruby-1.9.2-p320.tar.gz
+cd /usr/local/src/ruby-1.9.2-p320/
+sudo ./configure
+sudo make
+sudo checkinstall --pkgname ruby_1.9.2 --pkgversion 1.0 --default
+sudo gem install bundler  --no-ri --no-rdoc
+sudo gem install chef -v 11.6.0 --no-ri --no-rdoc 
+sudo gem install ruby-shadow --no-ri --no-rdoc
 git clone --recursive https://github.com/CartoDB/cartodb20.git /home/ubuntu/cartodb/cartodb20
 sudo add-apt-repository -y ppa:cartodb/gis
 sudo add-apt-repository -y ppa:cartodb/nodejs
 sudo add-apt-repository -y ppa:cartodb/redis
 sudo add-apt-repository -y ppa:cartodb/postgresql
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-sudo add-apt-repository -y ppa:mapnik/v2.1.0
+sudo add-apt-repository -y ppa:mapnik/nightly-2.1
 sudo apt-get -y update
 sudo git clone https://github.com/strtwtsn/CartoDB_NEW.git /var/chef/cartodb
-rvmsudo chef-solo -c /var/chef/cartodb/config/chefsolo.rb -j /var/chef/cartodb/roles/cartodb.json
+sudo chef-solo -c /var/chef/cartodb/config/chefsolo.rb -j /var/chef/cartodb/roles/cartodb.json
