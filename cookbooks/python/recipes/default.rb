bdb4906f50556e8b3fa1933732e3abdc69b62470
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 10:25:16 +0000

    Update default.rb

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -5,10 +5,6 @@ bash "Do python stuff" do
 user "root"
 code <<-EOH
 easy_install pip
-pip install --no-install GDAL
-cd /tmp/pip_build_root/GDAL/
-python /tmp/pip_build_root/GDAL/setup.py build_ext --include-dirs=/usr/include/gdal/
-pip install --no-download GDAL
 pip install -r /home/ubuntu/cartodb/cartodb20/python_requirements.txt
 pip install -e git+https://github.com/RealGeeks/python-varnish.git@0971d6024fbb2614350853a5e0f8736ba3fb1f0d#egg=python-varnish
 EOH
