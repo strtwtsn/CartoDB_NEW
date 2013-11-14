3468420ad2cb482f06871c75546501614dcd5494
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 15:42:09 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/ubuntu/cartodb/cartodb20
+sudo bundle install
+sudo redis-server /etc/redis/redis.conf
+echo "Please enter your Subdomain: "
+read SUBDOMAIN
+echo "Please enter your Password: "
+read PASSWORD
+echo "Please enter your Admin Password: "
+read ADMIN_PASSWORD
+echo "Please enter your Email Addresss: "
+read EMAIL
+sudo bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" EMAIL="${EMAIL}"
+sudo bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" EMAIL="${EMAIL}"
+echo "127.0.0.1 ${SUBDOMAIN}.localhost.lan" | sudo tee -a /etc/hosts
+sudo cp /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js.example /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js
+sudo cp /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js.example /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js
