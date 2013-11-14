6947fa931bfe2af77a6acadf94512aa8923abd91
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 16:35:36 +0000

    Update default.rb

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -6,7 +6,21 @@ mkdir /etc/redis
 EOH
 end
 
+service "redis" do
+  supports :restart => true, :start => true, :stop => true, :reload => true
+  action :nothing
+end 
 
 template "/etc/redis/redis.conf" do
 source "redis.conf.erb"
+end
+
+
+template "/etc/init.d/redis" do
+source "redis.erb"
+owner "root"
+group "root"
+mode "0755"
+notifies :enable, "service[redis]"
+notifies :start, "service[redis]"
 end
