b88511c0c1d77d234892f4fd537af644e83a0f76
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 15:49:49 +0000

    Update default.rb

--- cookbooks/pgbouncer/recipes/default.rb
+++ cookbooks/pgbouncer/recipes/default.rb
@@ -1,19 +1,11 @@
 package "pgbouncer"
 
-service "pgbouncer" do
-  service_name "pgbouncer"
-  supports :restart => true, :status => true, :reload => true
-end
-
-
 template "/etc/pgbouncer/pgbouncer.ini" do
 source "pgbouncer.ini.erb"
-notifies :restart, resources(:service => "pgbouncer"), :immediately
 end
 
 template "/etc/pgbouncer/userlist.txt" do
 source "userlist.txt.erb"
-notifies :restart, resources(:service => "pgbouncer"), :immediately
 end
 
 bash "Change port from 5432 to 6432" do
@@ -22,6 +14,9 @@ code <<-EOH
 sed -i 's/5432/6432/g' /home/ubuntu/cartodb20/config/database.yml
 chmod -R 755 /etc/pgbouncer/pgbouncer.ini
 chmod -R 755 /etc/pgbouncer/userlist.txt
+mkdir -p /var/log/pgbouncer
+touch /var/log/pgbouncer/pgbouncer.log
+chown -R postgres:postgres /var/log/pgbouncer
+chmod -r 777 /var/log/pgbouncer
 EOH
 end
-
