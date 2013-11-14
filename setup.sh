bb8ace8bb18633c5d720ad88d028a40dfdcb1448
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-14 16:17:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,3 +15,7 @@ sudo bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASS
 echo "127.0.0.1 ${SUBDOMAIN}.localhost.lan" | sudo tee -a /etc/hosts
 sudo cp /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js.example /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js
 sudo cp /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js.example /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js
+cd /etc/apache2/sites-available
+mv cartodb-temp ${SUBDOMAIN}
+sed -i 's/SUBDOMAIN/${SUBDOMAIN}/g' /etc/apache2/sites-available/${SUBDOMAIN}
+sudo a2ensite ${SUBDOMAIN}
