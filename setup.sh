43ba97cfd16bf052181ca4578bd9fda02cb7cf37
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-15 14:16:37 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+!/bin/bash
 cd /home/ubuntu/cartodb/cartodb20
 sudo bundle install
 sudo redis-server /etc/redis/redis.conf
+mv /home/ubuntu/cartodb/cartodb20/config/database.yml.sample  /home/ubuntu/cartodb/cartodb20/config/database.yml
+mv /home/ubuntu/cartodb/cartodb20/config/app_config.yml.sample  /home/ubuntu/cartodb/cartodb20/config/app_config.yml
 echo "Please enter your Subdomain: "
 read SUBDOMAIN
 echo "Please enter your Password: "
@@ -15,11 +17,9 @@ sudo bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASS
 echo "127.0.0.1 ${SUBDOMAIN}.localhost.lan" | sudo tee -a /etc/hosts
 sudo cp /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js.example /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/development.js
 sudo cp /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js.example /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/development.js
+SUBDOMAIN=development
 cd /etc/apache2/sites-available
-mv cartodb-temp ${SUBDOMAIN}
+sudo mv cartodb-temp ${SUBDOMAIN}
 sed -i 's/SUBDOMAIN/${SUBDOMAIN}/g' /etc/apache2/sites-available/${SUBDOMAIN}
 sudo a2ensite ${SUBDOMAIN}
-sudo service apache2 restart
-sudo service windshaft start
-sudo service cartodb start
-sudo service resque start
+sudo service apache2 start
