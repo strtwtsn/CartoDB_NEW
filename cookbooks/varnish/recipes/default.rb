39a7bbe0133bcc029b4f83bdac40f9c0ee290851
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-18 17:16:37 +0000

    Update default.rb

--- cookbooks/varnish/recipes/default.rb
+++ cookbooks/varnish/recipes/default.rb
@@ -1,2 +1,15 @@
 package "varnish"
 
+service "varnish" do
+  supports :restart => true, :start => true, :stop => true, :reload => true
+  action :nothing
+end 
+
+template "/etc/varnish/default.vcl" do
+source "default.vcl.erb"
+owner "root"
+group "root"
+mode "0755"
+notifies :enable, "service[varnish]"
+notifies :start, "service[varnish]"
+end
