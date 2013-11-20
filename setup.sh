cccfe617d5fe2dd85e3c4d952484ea488f9a549a
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-20 15:31:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ sudo redis-server /etc/redis/redis.conf
 mv /home/ubuntu/cartodb/cartodb20/config/database.yml.sample  /home/ubuntu/cartodb/cartodb20/config/database.yml
 echo "Please enter your Subdomain: "
 read SUBDOMAIN
+echo "Please enter your domain: "
+read DOMAIN
 echo "Please enter your Password: "
 read PASSWORD
 echo "Please enter your Admin Password: "
@@ -13,7 +15,7 @@ echo "Please enter your Email Addresss: "
 read EMAIL
 sudo RAILS_ENV=production bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" EMAIL="${EMAIL}"
 sudo RAILS_ENV=production bundle exec rake cartodb:db:setup SUBDOMAIN="${SUBDOMAIN}" PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" EMAIL="${EMAIL}"
-echo "127.0.0.1 ${SUBDOMAIN}.localhost.lan" | sudo tee -a /etc/hosts
+echo "127.0.0.1 ${SUBDOMAIN}.${DOMAIN}" | sudo tee -a /etc/hosts
 sudo cp /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/production.js.example /home/ubuntu/cartodb/CartoDB-SQL-API/config/environments/production.js
 sudo cp /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/production.js.example /home/ubuntu/cartodb/Windshaft-cartodb/config/environments/production.js
 SUBDOMAIN=${SUBDOMAIN}
@@ -21,6 +23,7 @@ cd /opt/nginx/sites-available
 sudo mv cartodb-temp ${SUBDOMAIN}
 sed -i 's/SUBDOMAIN/${SUBDOMAIN}/g' /opt/nginx/sites-available/${SUBDOMAIN}
 sed -i 's/SUBDOMAIN/${SUBDOMAIN}/g' /etc/pgbouncer/userlist.txt
+sed -i 's/localhost.lan/${DOMAIN}/g' /home/ubuntu/cartodb/*
 ln -s /opt/nginx/sites-available/${SUBDOMAIN} /opt/nginx/sites-enabled/${SUBDOMAIN}
 sudo service nginx restart
 
